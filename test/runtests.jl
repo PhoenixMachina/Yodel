c43b2e4fe55e1b0ff19ac0da2b6e62c977f4a07c
Nathanaël Langlois <nathanael.langlois@gmail.Com>
2016-03-03 11:03:44 +0100

    Added tests for YodelEngine constructor and routing processing

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,12 @@
 using Base.Test
 using Yodel
 
-@test 1==1
+ydl = YodelEngine(string(dirname(Base.source_path()),"/routes.xml"))
+
+r = getRouteWithController(ydl,"Home")
+@test r.url == "home"
+@test r.variables == []
+
+r2 = getRoute(ydl,"news/761546/")
+@test r2.controller == "news"
+@test r2.variables == ["article_id"]
